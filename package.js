67dfe22f759f81d4086f69efe828a107bf664da6
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-11-16 23:30:00 +0530

    refactored codebase and added http support

--- package.js
+++ package.js
@@ -7,12 +7,13 @@ Npm.depends({
 });
 
 Package.on_use(function(api) {
-  api.use(['minimongo', 'livedata', 'mongo-livedata', 'ejson', 'underscore'], ['server']);
+  api.use(['minimongo', 'livedata', 'mongo-livedata', 'ejson', 'underscore', 'http'], ['server']);
   api.add_files([
     'lib/notification_manager.js',
     'lib/wrap_session.js',
-    'lib/hijack_session.js',
-    'lib/hijack_db.js',
+    'lib/hijack/session.js',
+    'lib/hijack/db.js',
+    'lib/hijack/http.js',
     'lib/test_methods.js'
   ], 'server');
 });
