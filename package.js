b9ce326ab9812d43d8202719ce33396220901648
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-11-19 10:53:10 +0530

    refactord methods specific codebase into a model

--- package.js
+++ package.js
@@ -10,13 +10,14 @@ Npm.depends({
 Package.on_use(function(api) {
   api.use(['minimongo', 'livedata', 'mongo-livedata', 'ejson', 'underscore', 'http'], ['server']);
   api.add_files([
+    'lib/models/methods.js',
+    'lib/apm.js',
     'lib/notification_manager.js',
-    'lib/wrap_session.js',
+    'lib/hijack/wrap_session.js',
     'lib/hijack/session.js',
     'lib/hijack/db.js',
     'lib/hijack/http.js',
     'lib/hijack/async.js',
-    'lib/apm.js',
     'lib/test_methods.js'
   ], 'server');
   
