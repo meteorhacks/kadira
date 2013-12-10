229f382f4cecadf6035754de8079362c8d3eaffc
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-12-10 15:55:26 +0530

    replace max-min support with every event for every method, currently only implemented for max events

--- test/tests/models/methods.js
+++ test/tests/models/methods.js
@@ -77,21 +77,78 @@ suite('Methods Model', function() {
 
     server.evalSync(createMethodCompleted, 'aa', 'hello', 1, 100, 5);
     server.evalSync(createMethodCompleted, 'aa', 'hello', 2, 800 , 15);
+
     var payload1 = server.evalSync(getPayload);
     assert.deepEqual(payload1.methodRequests, []);
 
-    server.evalSync(createMethodCompleted, 'aa', 'hello', 3, 900 , 3);
-    var payload2 = server.evalSync(getPayload);  
-    assert.deepEqual(pick(payload2.methodRequests[0], ['_id', 'type']), {
-      _id: 'aa::2',
-      type: 'max'
-    });
-    assert.deepEqual(pick(payload2.methodRequests[1], ['_id', 'type']), {
-      _id: 'aa::3',
-      type: 'min'
-    });
-    assert.equal(payload2.methodRequests.length, 2);
+    server.evalSync(createMethodCompleted, 'aa', 'hello', 3, 900 , 2000);
+    var lastMethodId = 3;
+    var expectedResult = {
+      total: {_id: 'aa::3', total: 2000}
+    };
 
+    ['wait', 'http', 'db', 'email', 'async'].forEach(function(eventType) {
+      server.evalSync(createMethodWithEvent, 'aa', 'hello', ++lastMethodId, 800 , eventType, 1000);
+      expectedResult[eventType] = {_id: 'aa::' + lastMethodId};
+      expectedResult[eventType][eventType] = 1000;
+      server.evalSync(createMethodWithEvent, 'aa', 'hello', ++lastMethodId, 800 , eventType, 10);
+    });
+
+    var payload2 = server.evalSync(getPayload);  
+    assert.equal(payload2.methodRequests.length, 6);
+    
+    var processedResult = {};
+    payload2.methodRequests.forEach(function(method) {
+      processedResult[method.event] = pick(method, ['_id', method.event]);
+    });
+
+    assert.deepEqual(processedResult, expectedResult);
+    done();
+  });
+
+  test('buildPayload - max min - multi-methods', function(done, server) {
+    server.evalSync(function() {
+      model = new MethodsModel();
+      model.sendMaxMinInterval = 2;
+      emit('return');
+    });
+
+    server.evalSync(createMethodCompleted, 'aa', 'hello', 1, 100, 5);
+    server.evalSync(createMethodCompleted, 'aa', 'hello', 2, 800 , 15);
+
+    var payload1 = server.evalSync(getPayload);
+    assert.deepEqual(payload1.methodRequests, []);
+
+    server.evalSync(createMethodCompleted, 'aa', 'hello', 3, 900 , 2000);
+    server.evalSync(createMethodCompleted, 'aa', 'hi', 200, 900 , 2000);
+    var lastMethodId = 3;
+    var expectedResult = {
+      "hello::total": {_id: 'aa::3', total: 2000},
+      "hi::total": {_id: 'aa::200', total: 2000},
+    };
+
+    ['hello', 'hi'].forEach(function(methodName) {
+      ['wait', 'http', 'db', 'email', 'async'].forEach(function(eventType) {
+        server.evalSync(createMethodWithEvent, 'aa', methodName, ++lastMethodId, 800 , eventType, 1000);
+
+        var id = methodName + "::" + eventType;
+        expectedResult[id] = {_id: 'aa::' + lastMethodId};
+        expectedResult[id][eventType] = 1000;
+
+        server.evalSync(createMethodWithEvent, 'aa', methodName, ++lastMethodId, 800 , eventType, 10);
+      });
+    });
+
+    var payload2 = server.evalSync(getPayload);  
+    assert.equal(payload2.methodRequests.length, 12);
+    
+    var processedResult = {};
+    payload2.methodRequests.forEach(function(method) {
+      var id = method.name + "::" + method.event;
+      processedResult[id] = pick(method, ['_id', method.event]);
+    });
+
+    assert.deepEqual(processedResult, expectedResult);
     done();
   });
 });
@@ -109,6 +166,18 @@ function createMethodCompleted(sessionName, methodName, methodId, startTime, met
   var method = model.getMethod({session: sessionName, method: {name: methodName, id: methodId}});
   method.events.push({type: 'start', at: startTime});
   method.events.push({type: 'complete', at: startTime + methodDelay});
+
+  emit('return');
+}
+
+function createMethodWithEvent(sessionName, methodName, methodId, startTime, eventName, eventDelay) {
+  var time = startTime;
+
+  var method = model.getMethod({session: sessionName, method: {name: methodName, id: methodId}});
+  method.events.push({type: 'start', at: time});
+  method.events.push({type: eventName, at: time += 5});
+  method.events.push({type: eventName + 'end', at: time += eventDelay});
+  method.events.push({type: 'complete', at: time += 5});
 
   emit('return');
 }
