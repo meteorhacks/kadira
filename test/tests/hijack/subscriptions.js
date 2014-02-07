ab9a3aa4c3489dacda1fd0c1f11aa781425b9645
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-02-07 12:55:48 +0530

    changed dataImpact => networkImpact added tests

--- test/tests/hijack/subscriptions.js
+++ test/tests/hijack/subscriptions.js
@@ -121,14 +121,149 @@ suite('Hijack - Subscriptions', function() {
       }); 
     });
 
-
-    server.evalSync(function() {
-      emit('return');
-    });
-
     var payload = GetPubsubPayload(server);
     assert.equal(payload[0].pubs.postsList.count, 2);
     done();
   });
 
+  test('networkImpact:added', function(done, server, client) {
+    //_id is not calculated
+    var docs = [
+      {data: 'data1'},
+      {data: 'data2'}
+    ];
+
+    server.evalSync(function(docs) {
+      Posts = new Meteor.Collection('posts');
+      docs.forEach(function(doc) {
+        Posts.insert(doc);
+      });
+
+      Meteor.publish('postsList', function() {
+        return Posts.find();
+      });
+      emit('return');
+    }, docs);
+
+    client.evalSync(function() {
+      var h1 = Meteor.subscribe('postsList', function() {
+        emit('return');
+      }); 
+    });
+
+    Wait(server, 200);
+
+    var metrics = GetPubsubMetrics(server);
+    assert.equal(metrics[0].pubs.postsList.networkImpact, getDataSize(docs));
+    done();
+  });
+
+  test('networkImpact:added multi-subscriptions', function(done, server, client) {
+    //_id is not calculated
+    var docs = [
+      {data: 'data1'},
+      {data: 'data2'}
+    ];
+
+    server.evalSync(function(docs) {
+      Posts = new Meteor.Collection('posts');
+      docs.forEach(function(doc) {
+        Posts.insert(doc);
+      });
+
+      Meteor.publish('postsList', function() {
+        return Posts.find();
+      });
+      emit('return');
+    }, docs);
+
+    client.evalSync(function() {
+      var h1 = Meteor.subscribe('postsList', function() {
+        var h2 = Meteor.subscribe('postsList', function() {
+          emit('return');
+        });
+      }); 
+    });
+
+    Wait(server, 200);
+
+    var metrics = GetPubsubMetrics(server);
+    assert.equal(metrics[0].pubs.postsList.networkImpact, getDataSize(docs));
+    done();
+  });
+  
+  test('networkImpact:updated', function(done, server, client) {
+    var docs = [
+      {abc: 10},
+      {bbc: 20}
+    ];
+    server.evalSync(function() {
+      Posts = new Meteor.Collection('posts');
+      Posts.insert({_id: 'aa', abc: 10});
+
+      Meteor.publish('postsList', function() {
+        return Posts.find();
+      });
+      emit('return');
+    }, docs);
+
+    client.evalSync(function() {
+      var h1 = Meteor.subscribe('postsList', function() {
+        emit('return');
+      }); 
+    });
+
+    server.evalSync(function() {
+      Posts.update({_id: 'aa'}, {$set: {bbc: 10}});
+      emit('return');
+    });
+
+    Wait(server, 200);
+
+    var metrics = GetPubsubMetrics(server);
+    assert.equal(metrics[0].pubs.postsList.networkImpact, getDataSize(docs));
+    done();
+  });
+
+  test('networkImpact:removed', function(done, server, client) {
+    var docs = [
+      {abc: 10}
+    ];
+    server.evalSync(function() {
+      Posts = new Meteor.Collection('posts');
+      Posts.insert({_id: 'aa', abc: 10});
+
+      Meteor.publish('postsList', function() {
+        return Posts.find();
+      });
+      emit('return');
+    }, docs);
+
+    client.evalSync(function() {
+      var h1 = Meteor.subscribe('postsList', function() {
+        emit('return');
+      }); 
+    });
+
+    server.evalSync(function() {
+      Posts.remove({_id: 'aa'});
+      emit('return');
+    });
+
+    Wait(server, 200);
+
+    var metrics = GetPubsubMetrics(server);
+    assert.equal(metrics[0].pubs.postsList.networkImpact, getDataSize(docs));
+    done();
+  });
+
 });
+
+
+function getDataSize(docs) {
+  var size = 0;
+  docs.forEach(function(doc) {
+    size+= Buffer.byteLength(JSON.stringify(doc));
+  });
+  return size;
+}
