781a1d833e3a1d4cd37b5a039bce96d99ef7aaf1
Muhammed Thanish <mnmtanish@gmail.com>
2014-05-07 08:34:15 +0530

    Remove Async.wrap tests

--- tests/hijack/async.js
+++ tests/hijack/async.js
@@ -19,7 +19,7 @@ Tinytest.add(
       ['complete']
     ];
     test.equal(events, expected);
-    CleanTestData('methodstore');
+    CleanTestData();
   }
 );
 
@@ -49,71 +49,6 @@ Tinytest.add(
       ['complete']
     ];
     test.equal(events, expected);
-    CleanTestData('methodstore');
-  }
-);
-
-Tinytest.add(
-  'Async - track with Async.wrap with error',
-  function (test) {
-    test.fail('Async not defined');
-    // EnableTrackingMethods();
-    // var methodId = RegisterMethod(function () {
-    //   var wait = Async.wrap(function(waitTime, callback) {
-    //     setTimeout(function () {
-    //       callback(new Error('error'));
-    //     }, waitTime);
-    //   });
-    //   try {
-    //     wait(100);
-    //   } catch (ex) {
-
-    //   }
-    // });
-    // var client = GetMeteorClient();
-    // var result = client.call(methodId);
-    // var events = GetLastMethodEvents([0]);
-    // var expected = [
-    //   ['start'],
-    //   ['wait'],
-    //   ['async'],
-    //   ['complete']
-    // ];
-    // test.equal(events, expected);
-    // CleanTestData('methodstore');
-  }
-);
-
-Tinytest.add(
-  'Async - track with Async.wrap with error and continue',
-  function (test) {
-    test.fail('Async not defined');
-    // EnableTrackingMethods();
-    // var methodId = RegisterMethod(function () {
-    //   var wait = Async.wrap(function(waitTime, callback) {
-    //     setTimeout(function () {
-    //       callback(new Error('error'));
-    //     }, waitTime);
-    //   });
-    //   try {
-    //     wait(100);
-    //   } catch (ex) {
-
-    //   }
-    //   TestData.find({}).fetch();
-    // });
-    // var client = GetMeteorClient();
-    // var result = client.call(methodId);
-    // var events = GetLastMethodEvents([0, 2]);
-    // var expected = [
-    //   ['start',, {userId: null, params: '[]'}],
-    //   ['wait',, {waitOn: []}],
-    //   ['async',, {}],
-    //   ['db',, {coll: 'tinytest-data', func: 'find', selector: JSON.stringify({})}],
-    //   ['db',, {coll: 'tinytest-data', func: 'fetch', cursor: true, selector: JSON.stringify({}), docsFetched: 0}],
-    //   ['complete']
-    // ];
-    // test.equal(events, expected);
-    // CleanTestData('methodstore', 'testdata');
+    CleanTestData();
   }
 );
