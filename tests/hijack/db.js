0cd4cdcd88099df288dfd2237ac25b52250cd347
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-05-25 16:24:18 +0530

    checking for upsert return object existance if called with an callback, it could be null

--- tests/hijack/db.js
+++ tests/hijack/db.js
@@ -138,16 +138,16 @@ Tinytest.add(
       ['wait',,{waitOn: []}],
       ['db',,{coll: 'tinytest-data', func: 'find', selector: JSON.stringify({_id: 'aa'})}],
       ['db',,{
-        coll: 'tinytest-data', 
-        func: 'fetch', 
-        cursor: true, 
-        selector: JSON.stringify({_id: 'aa'}), 
+        coll: 'tinytest-data',
+        func: 'fetch',
+        cursor: true,
+        selector: JSON.stringify({_id: 'aa'}),
         docsFetched: 1,
         limit: 1
       }],
       ['complete']
     ];
-  
+
     test.equal(result, {_id: 'aa', dd: 10});
     test.equal(events, expected);
     CleanTestData();
@@ -173,18 +173,18 @@ Tinytest.add(
       ['wait',,{waitOn: []}],
       ['db',,{coll: 'tinytest-data', func: 'find', selector: JSON.stringify({_id: 'aa'})}],
       ['db',,{
-        coll: 'tinytest-data', 
-        func: 'fetch', 
-        cursor: true, 
-        selector: JSON.stringify({_id: 'aa'}), 
-        sort: JSON.stringify({dd: -1}), 
-        fields: JSON.stringify({dd: 1}), 
+        coll: 'tinytest-data',
+        func: 'fetch',
+        cursor: true,
+        selector: JSON.stringify({_id: 'aa'}),
+        sort: JSON.stringify({dd: -1}),
+        fields: JSON.stringify({dd: 1}),
         docsFetched: 1,
         limit: 1
       }],
       ['complete']
     ];
-  
+
     test.equal(result, {_id: 'aa', dd: 10});
     test.equal(events, expected);
     CleanTestData();
@@ -208,6 +208,30 @@ Tinytest.add(
       ['wait',,{waitOn: []}],
       ['db',,{coll: 'tinytest-data', func: 'upsert', selector: JSON.stringify({_id: 'aa'}), updatedDocs: 1, insertedId: 'aa'}],
       ['db',,{coll: 'tinytest-data', func: 'upsert', selector: JSON.stringify({_id: 'aa'}), updatedDocs: 1, insertedId: undefined}],
+      ['complete']
+    ];
+    test.equal(events, expected);
+    CleanTestData();
+  }
+);
+
+Tinytest.add(
+  'Database - upsert with update',
+  function (test) {
+    EnableTrackingMethods();
+    var methodId = RegisterMethod(function () {
+      TestData.update({_id: 'aa'}, {$set: {bb: 20}}, {upsert: true});
+      TestData.update({_id: 'aa'}, {$set: {bb: 30}}, {upsert: true});
+      return 'upsert';
+    });
+    var client = GetMeteorClient();
+    var result = client.call(methodId);
+    var events = GetLastMethodEvents([0, 2]);
+    var expected = [
+      ['start',,{userId: null, params: '[]'}],
+      ['wait',,{waitOn: []}],
+      ['db',,{coll: 'tinytest-data', func: 'upsert', selector: JSON.stringify({_id: 'aa'}), updatedDocs: 1}],
+      ['db',,{coll: 'tinytest-data', func: 'upsert', selector: JSON.stringify({_id: 'aa'}), updatedDocs: 1}],
       ['complete']
     ];
     test.equal(events, expected);
