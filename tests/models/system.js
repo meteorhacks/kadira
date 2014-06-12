30ee21e439384b5641ecbd6e16875e19601b9b41
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-06-12 05:51:06 +0530

    added cputime and rename cpu as pcpu

--- tests/models/system.js
+++ tests/models/system.js
@@ -8,9 +8,9 @@ Tinytest.add(
       setTimeout(callback, 1000);
     })();
     var payload = model.buildPayload().systemMetrics[0];
-    
+
     test.isTrue(payload.memory > 0);
-    test.isTrue(payload.cpu > 0);
+    test.isTrue(payload.pcpu > 0);
     test.isTrue(payload.sessions >= 0);
     test.isTrue(payload.startTime >= beforeTime);
     test.isTrue(payload.endTime >= payload.startTime + 1000);
