76b1bc636f279f7257904f4e7648f7ca5f1a2550
Muhammed Thanish <mnmtanish@gmail.com>
2014-07-17 12:44:08 +0530

    Send error trace (without events) for zone errors

--- package.js
+++ package.js
@@ -46,6 +46,7 @@ Package.on_test(function(api) {
 function configurePackage(api) {
   api.use(['minimongo', 'livedata', 'mongo-livedata', 'ejson', 'underscore', 'http', 'email', 'random'], ['server']);
   api.use(['underscore', 'random', 'jquery', 'localstorage'], ['client']);
+  api.use('zones');
   api.add_files([
     'lib/retry.js',
     'lib/utils.js',
@@ -74,5 +75,6 @@ function configurePackage(api) {
     'lib/client/route.js',
     'lib/client/stacktrace.js',
     'lib/client/kadira.js',
+    'lib/client/zone-reporter.js',
   ], 'client')
 }
