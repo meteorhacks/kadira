56870fe285c70dd09328fdf63c67819b9479d912
Muhammed Thanish <mnmtanish@gmail.com>
2014-07-25 15:16:01 +0530

    Test getCurrentOrigin() and formatTraceLine()

--- tests/client/utils.js
+++ tests/client/utils.js
@@ -7,3 +7,34 @@ Tinytest.add(
     test.equal(stackFramesFilter('/packages/foo/bar.js'), true);
   }
 );
+
+Tinytest.add(
+  'Client Side - Error Manager - Utils - getCurrentOrigin()',
+  function (test) {
+    test.equal(typeof getCurrentOrigin, 'function');
+    // Simple regex to catch origin part of the url
+    // (may not work for all cases but sufficient for tests)
+    var regex = /^https?:\/\/[a-zA-Z0-9\.]+:?[0-9]*$/;
+    var origin = getCurrentOrigin();
+    test.equal(typeof origin, 'string');
+    test.equal(!!origin.match(regex), true);
+  }
+);
+
+Tinytest.add(
+  'Client Side - Error Manager - Utils - formatTraceLine()',
+  function (test) {
+    test.equal(typeof formatTraceLine, 'function');
+    var lines = [
+      '{anonymous}()@'+getCurrentOrigin()+'/foo/bar.js:12:34',
+      'funName@'+getCurrentOrigin()+'/foo/bar.js:12:34'
+    ];
+    var expected = [
+      '    at foo/bar.js:12:34',
+      '    at funName (foo/bar.js:12:34)',
+    ];
+    for(var i=lines.length; i-->0;) {
+      test.equal(expected[i], formatTraceLine(lines[i]));
+    }
+  }
+);
