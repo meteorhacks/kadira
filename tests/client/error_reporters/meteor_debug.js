f7c0585a8727996d74483dc197d279b0a79060fa
Muhammed Thanish <mnmtanish@gmail.com>
2014-08-05 16:05:43 +0530

    Catch errors with Meteor._debug
    If zone is available (which should be) catch the error and send it to servers

---
+++ tests/client/error_reporters/meteor_debug.js
@@ -0,0 +1,58 @@
+
+Tinytest.addAsync(
+  'Client Side - Error Manager - Reporters - meteor._debug - default',
+  function (test, next) {
+    hijackPrintStackTrace(mock_printStackTrace);
+    hijackKadiraSendErrors(mock_KadiraSendErrors);
+    test.equal(typeof Meteor._debug, 'function');
+    Meteor._debug('_error', '_stack');
+
+    function mock_KadiraSendErrors(data) {
+      test.equal(true, Array.isArray(data));
+      test.equal(1, data.length);
+      var error = data[0];
+      test.equal('string', typeof error.appId);
+      test.equal('object', typeof error.info);
+      test.equal('_error', error.name);
+      test.equal('client', error.source);
+      test.equal(true, Array.isArray(error.stacks));
+      test.equal('number', typeof error.startTime);
+      test.equal('meteor._debug', error.type);
+      restoreKadiraSendErrors();
+      restorePrintStackTrace();
+      next();
+    }
+  }
+);
+
+//--------------------------------------------------------------------------\\
+
+var original_printStackTrace = printStackTrace;
+
+function hijackPrintStackTrace(mock) {
+  printStackTrace = mock;
+}
+
+function restorePrintStackTrace() {
+  printStackTrace = original_printStackTrace;
+}
+
+function mock_printStackTrace() {
+  var o = getCurrentOrigin();
+  return [
+    '{anonymous}()@'+o+'/packages/zones/assets/bar.js:12:34',
+    'funName@'+o+'/packages/zones/assets/bar.js:12:34',
+    '{anonymous}()@'+o+'/foo/bar.js:12:34',
+    'funName@'+o+'/foo/bar.js:12:34'
+  ];
+}
+
+var original_KadiraSendErrors = Kadira.sendErrors;
+
+function hijackKadiraSendErrors(mock) {
+  Kadira.sendErrors = mock;
+}
+
+function restoreKadiraSendErrors() {
+  Kadira.sendErrors = original_KadiraSendErrors;
+}
