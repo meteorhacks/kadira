a64127788ddf81ea1e105557072ba1f1233e4931
Muhammed Thanish <mnmtanish@gmail.com>
2014-08-15 09:58:29 +0530

    Limit number of errors sent to kadira

--- tests/models/errors.js
+++ tests/models/errors.js
@@ -135,3 +135,37 @@ Tinytest.add(
     });
   }
 );
+
+Tinytest.add(
+  'Models - Errors - buildPayload with too much errors',
+  function (test) {
+    var model = new ErrorModel('_appId');
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].forEach(function(n) {
+      var error = {name: '_name'+n, message: '_message'+n, stack: '_stack'+n};
+      var trace = {type: '_type'+n, name: '_name'+n};
+      model.trackError(error, trace);
+    });
+
+    var metrics = model.buildPayload().errors;
+    test.isTrue(Array.isArray(metrics));
+    test.equal(metrics.length, 10);
+
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach(function(n) {
+      var payload = metrics[n-1];
+      var trace = {type: '_type'+n, name: '_name'+n};
+      var expected = {
+        appId: '_appId',
+        name: '_message'+n,
+        source: '_type'+n+':_name'+n,
+        // startTime: Date.now(),
+        type: 'server',
+        trace: trace,
+        stacks: [{stack: '_stack'+n}],
+        count: 1,
+      };
+      test.equal(typeof payload.startTime, 'number');
+      delete payload.startTime;
+      test.equal(payload, expected);
+    });
+  }
+);
