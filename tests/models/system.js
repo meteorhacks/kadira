0585bbfe6c97d10c65eb98b5a4b1c466aecdf786
Muhammed Thanish <mnmtanish@gmail.com>
2014-10-10 14:06:15 +0530

    Add tests

--- tests/models/system.js
+++ tests/models/system.js
@@ -26,6 +26,15 @@ Tinytest.add(
 );
 
 Tinytest.add(
+  'Models - System - new Sessions - ignore local sessions',
+  function (test) {
+    var model = new SystemModel();
+    model.handleSessionActivity({msg: 'connect', _isLocalhost: true}, 'id');
+    test.equal(model.newSessions, 0);
+  }
+);
+
+Tinytest.add(
   'Models - System - new Sessions - multiple sessions',
   function (test) {
     var model = new SystemModel();
@@ -64,7 +73,7 @@ Tinytest.add(
     model.handleSessionActivity({msg: 'connect'}, 'the-new-id');
     model.handleSessionActivity({msg: 'sub'}, 'the-new-id');
     model.sessionTimeout = 100;
-    
+
     Wait(200);
     model.handleSessionActivity({msg: 'sub'}, 'the-new-id');
     test.equal(model.newSessions, 2);
@@ -124,8 +133,8 @@ Tinytest.add(
     var model = Kadira.models.system;
     var initCount = model.newSessions;
 
-    GetMeteorClient();
-    GetMeteorClient();
+    sendConnectMessage({remoteAddress: '1.1.1.1'});
+    sendConnectMessage({remoteAddress: '1.1.1.1'});
 
     Wait(100);
     var newSessions = model.newSessions - initCount;
@@ -139,14 +148,49 @@ Tinytest.add(
     var model = Kadira.models.system;
     var initCount = model.newSessions;
 
-    var client = GetMeteorClient();
+    var sessionId = sendConnectMessage({remoteAddress: '1.1.1.1'});
     Wait(50);
-    client.disconnect();
-    Wait(50);
-    client.reconnect();
+    sendConnectMessage({remoteAddress: '1.1.1.1', sessionId: sessionId});
     Wait(50);
 
     var newSessions = model.newSessions - initCount;
     test.equal(newSessions, 1);
   }
 );
+
+Tinytest.add(
+  'Models - System - new Sessions - integration - local connection',
+  function (test) {
+    var model = Kadira.models.system;
+    var initCount = model.newSessions;
+
+    sendConnectMessage({remoteAddress: '127.0.0.1'});
+    sendConnectMessage({forwardedAddress: '127.0.0.1'});
+
+    Wait(100);
+    var newSessions = model.newSessions - initCount;
+    test.equal(newSessions, 0);
+  }
+);
+
+function sendConnectMessage (options) {
+  var socket = {send: function() {}, close: function() {}, headers: []};
+  if(options.remoteAddress)
+    socket.remoteAddress = options.remoteAddress;
+  if(options.forwardedAddress)
+    socket.headers['x-forwarded-for'] = options.forwardedAddress;
+
+  var message = {msg: 'connect', version: 'pre1', support: ['pre1']};
+  if(options.sessionId)
+    message.session = options.sessionId;
+
+  var session = null;
+  var _originalOnConnection = Meteor.onConnection;
+  Meteor.onConnection = function (cb) {
+    return _originalOnConnection(function (s) { session = s; cb(s); });
+  }
+
+  Meteor.default_server._handleConnect(socket, message);
+  Meteor.onConnection = _originalOnConnection;
+  return session.id;
+}
