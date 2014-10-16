6577e3e5f0434a8b0dafce417e3af3bcd2b9e7d0
Muhammed Thanish <mnmtanish@gmail.com>
2014-10-16 13:07:58 +0530

    Label mongodb.Connection.createDataHandler

--- tests/profile/set_labels.js
+++ tests/profile/set_labels.js
@@ -4,3 +4,12 @@ Tinytest.add(
     test.equal(MeteorX.Session.prototype.send.name, 'kadira_Session_send');
   }
 );
+
+Tinytest.add(
+  'CPU Profiler - set labels - mongodb.Connection.createDataHandler',
+  function (test) {
+    var mongodb = MongoInternals.NpmModule;
+    var handler = mongodb.Connection.createDataHandler();
+    test.equal(handler.name, 'kadira_MongoDB_dataHandler');
+  }
+);
