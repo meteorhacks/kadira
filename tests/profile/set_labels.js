96e366f4861adabed86dd127e4dcabb4cafafcc3
Muhammed Thanish <mnmtanish@gmail.com>
2014-10-16 13:09:48 +0530

    Label MongoCursor methods

--- tests/profile/set_labels.js
+++ tests/profile/set_labels.js
@@ -13,3 +13,14 @@ Tinytest.add(
     test.equal(handler.name, 'kadira_MongoDB_dataHandler');
   }
 );
+
+Tinytest.add(
+  'CPU Profiler - set labels - MongoCursor methods',
+  function (test) {
+    var cursorProto = MeteorX.MongoCursor.prototype;
+    ['forEach', 'map', 'fetch', 'count', 'observeChanges', 'observe', 'rewind']
+    .forEach(function (name) {
+      test.equal(cursorProto[name].name, 'kadira_Cursor_'+name);
+    });
+  }
+);
