6fd33d3f8c9430836fd27fab6eb1d998f1f161c9
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-11-12 15:57:26 +0530

    version bumped

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Performance Monitoring for Meteor",
-  "version": "2.12.2",
+  "version": "2.13.0",
   "git": "https://github.com/meteorhacks/kadira.git",
   "name": "meteorhacks:kadira"
 });
