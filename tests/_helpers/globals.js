3ab418b876fa6607c2337ba0300ee0782700e4f2
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-14 21:43:36 +0530

    Use Meteor.Collection for backward compatibility

--- tests/_helpers/globals.js
+++ tests/_helpers/globals.js
@@ -1,3 +1,3 @@
 MethodStore = [];
-TestData = new Mongo.Collection('tinytest-data');
-TestData2 = new Mongo.Collection('tinytest-data-2');
+TestData = new Meteor.Collection('tinytest-data');
+TestData2 = new Meteor.Collection('tinytest-data-2');
