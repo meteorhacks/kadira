e99846c217d704567111411b90f40e800f0b3b1b
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-10-06 16:50:23 +0530

    Add meteorx support to client only

--- package.js
+++ package.js
@@ -83,7 +83,7 @@ Package.on_test(function(api) {
 function configurePackage(api) {
   if(api.versionsFrom) {
     api.versionsFrom('METEOR@1.0');
-    api.use('meteorhacks:meteorx@1.4.1');
+    api.use('meteorhacks:meteorx@1.4.1', ['server']);
     api.use('meteorhacks:zones@1.2.1', {weak: true});
   }
 
